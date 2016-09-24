1fed8332cf00e770e54f056256e13a4c3287380c
clemsciences <clemsciences@aol.com>
2016-09-25 00:58:21 +0200

    prise en compte d'une variable mise dans le template année température

--- arrosage_automatique/app_arrosage.py
+++ arrosage_automatique/app_arrosage.py
@@ -1,6 +1,7 @@
 # -*-coding:utf-8-*-
 from flask import Flask, render_template
 from arrosage_database_manager import RecuperateurDonnees
+import numpy as np
 import io
 import generateur_graphique_meteo
 
@@ -76,9 +77,12 @@ def get_temperature_annee(annee):
                        "novembre", "decembre"]
     temps, temperatures = recuperateur.obtenir_temprature_annee(annee)
     nom_image_min, nom_image_max, nom_image_moyenne = generateur_graphique_meteo.obtenir_courbe_temperature_annee(temps, temperatures, annee)
+    mois_presents = list(set([timme.month for timme in temps]))
+    moyennes_mensuels = [np.mean([tempe for j, tempe in enumerate(temperatures) if temps[j].month == i]) for i in mois_presents]
+
     return render_template("affichage_temperature_annee.html", l_indices_mois=l_indices_mois, mois=mois,
                            nom_image_min=nom_image_min, nom_image_max=nom_image_max,
-                           nom_image_moyenne=nom_image_moyenne)
+                           nom_image_moyenne=nom_image_moyenne, temperatures_moyennes_mois=moyennes_mensuels)
 
 
 
