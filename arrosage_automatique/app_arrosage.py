7515cc36eb4d54fcf06093f7a24c09a7f5424e5d
clemsciences <clemsciences@aol.com>
2016-09-25 01:09:09 +0200

    ascii != ce que je veux

--- arrosage_automatique/app_arrosage.py
+++ arrosage_automatique/app_arrosage.py
@@ -83,7 +83,7 @@ def get_temperature_annee(annee):
         if timme in mois_presents:
             truc_pour_page_web.append(np.mean([tempe for j, tempe in enumerate(temperatures) if temps[j].month == timme]))
         else:
-            truc_pour_page_web.append("non mesuré")
+            truc_pour_page_web.append("non mesure")
     return render_template("affichage_temperature_annee.html", l_indices_mois=l_indices_mois, mois=mois,
                            nom_image_min=nom_image_min, nom_image_max=nom_image_max,
                            nom_image_moyenne=nom_image_moyenne, temperatures_moyennes_mois=truc_pour_page_web, annee=annee)
