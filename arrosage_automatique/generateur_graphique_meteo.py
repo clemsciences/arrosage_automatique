692b596ab16616955f68da5aa259b71b01fed3e1
clemsciences <clemsciences@aol.com>
2016-09-25 00:23:50 +0200

    le bon chemin pour les images

--- arrosage_automatique/generateur_graphique_meteo.py
+++ arrosage_automatique/generateur_graphique_meteo.py
@@ -3,7 +3,7 @@ import numpy as np
 import datetime
 import matplotlib.pyplot as plt
 import os
-DIRECTORY = "images"
+DIRECTORY = "/home/pi/arrosage_automatique/arrosage_automatique/images"
 conversion_mois = ["janvier", "fevrier", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre",
                        "novembre", "décembre"]
 
